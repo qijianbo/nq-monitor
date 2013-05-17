e2e6366c57f3e9ba9d763873a000574ec850ff7a
sadpiss <sadpiss@gmail.com>
2013-05-17 08:58:05 +0000

    小有成就

--- web/js/index.js
+++ web/js/index.js
@@ -6,52 +6,38 @@ $(document).ready(function() {
             }
         });
 
-        var chart;
-        $('#virusUpdateRate').highcharts({
+        var chart =new Highcharts.Chart({
             chart: {
-                type: 'spline',
+                renderTo:"virusUpdateRate",
+                defaultSeriesType:'spline',
                 animation: Highcharts.svg, // don't animate in old IE
-                marginRight: 5,
-                events: {
-                    load: function() {
-                        // set up the updating of the chart each second
-                        var series = this.series[0];
-                        setInterval(function() {
-                            var x = (new Date()).getTime(), // current time
-                                y = Math.random();
-                            series.addPoint([x, y], true, true);
-                        }, 1000*3600);
-                    }
-                }
+                marginRight: 5
             },
             title: {
                 text: '海外病毒库更新成功率'
             },
+            tooltip: {
+                formatter: function() {
+                    return Highcharts.dateFormat('%Y-%m-%d %H:00', this.x) +'<br/>'+
+                        '<b>'+ this.series.name +'</b>:'+
+                        Highcharts.numberFormat(this.y, 2)+"%";
+                }
+            },
             xAxis: {
-                type: '',
+                title:"时间",
+                type: 'datetime',
                 labels: {
                     formatter: function() {
-                        return Highcharts.dateFormat("%H", this.value, false);
+                        return Highcharts.dateFormat('%H', this.value);
                     }
                 },
-                tickPixelInterval: 30     //x像素间隔
+                tickPixelInterval: 30
             },
             yAxis: {
                 title: {
-                    text: 'Value'
+                    text: '成功率'
                 },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                formatter: function() {
-                    return '<b>'+ this.series.name +'</b><br/>'+
-                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
-                        Highcharts.numberFormat(this.y, 2);
-                }
+                min:0
             },
             legend: {
                 enabled: false
@@ -60,23 +46,47 @@ $(document).ready(function() {
                 enabled: false
             },
             series: [{
-                name: 'Random data',
+                name:'成功率',
                 data: (function() {
-                    // generate an array of random data
                     var data = [],
-                        time = (new Date()).getTime(),
-                        i;
-                    for (i = -24*12; i <= 0; i++) {
-                        data.push({
-                            x: time + i*5* 60 * 1000,
-                            y: Math.random()
-                        });
+                        dateTime = (new Date()).getTime(),
+                        i,el,elTime;
+                    for (i = 24; i > 0; i--) {
+                        elTime =(new Date(dateTime-i*3600*1000));
+                        el = {
+                            x: elTime,
+                            y: getRandomDate(),
+                            name:elTime.Format('yyyy-MM-dd HH:00')
+                        };
+                        data.push(el);
+//                        alert(el.x+","+el.y)
                     }
                     return data;
                 })()
             }]
         });
     });
-
 });
+
+function getRandomDate(){
+    return 100;
+//    return parseInt(Math.random()*(100-80+1)+80)
+}
+
+Date.prototype.Format = function (fmt) { //author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "H+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
 
