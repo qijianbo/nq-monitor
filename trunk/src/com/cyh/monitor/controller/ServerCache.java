976aec69adfdb7813ebace5d480f484a2000f900
sadpiss@gmail.com <sadpiss@gmail.com@99931ce1-a2b7-12bf-9ff3-331b7be87867>
2013-05-17 08:58:05 +0000

    小有成就

---
+++ trunk/src/com/cyh/monitor/controller/ServerCache.java
@@ -0,0 +1,10 @@
+package com.cyh.monitor.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cyh
+ * Date: 13-5-17
+ * Time: 下午2:56
+ */
+public class ServerCache {
+}
---
+++ trunk/src/com/cyh/monitor/controller/ServerMonitor.java
@@ -0,0 +1,11 @@
+package com.cyh.monitor.controller;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cyh
+ * Date: 13-5-17
+ * Time: 下午2:56
+ */
+public class ServerMonitor extends MultiActionController {
+}
